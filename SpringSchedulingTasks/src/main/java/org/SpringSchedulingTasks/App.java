03f6df8d302659a00242631c2dd435be14e3bcf6
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-04-26 22:28:27 +0100

    App init

--- SpringSchedulingTasks/src/main/java/org/SpringSchedulingTasks/App.java
+++ SpringSchedulingTasks/src/main/java/org/SpringSchedulingTasks/App.java
@@ -1,13 +1,14 @@
 package org.SpringSchedulingTasks;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class);
+	}
 }
