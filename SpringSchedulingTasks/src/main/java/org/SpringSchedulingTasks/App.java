6bfa86e55fabdc9b725b932f9912b2e906f78de2
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-04-26 21:40:30 +0100

    Project creation

---
+++ SpringSchedulingTasks/src/main/java/org/SpringSchedulingTasks/App.java
@@ -0,0 +1,13 @@
+package org.SpringSchedulingTasks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
